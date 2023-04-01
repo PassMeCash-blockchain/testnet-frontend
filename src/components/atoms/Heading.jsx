a7bed4ae2d274a5b00f67801dc3d12207934192e
favour ebrusike <ebrusikefavour@gmail.com>
2023-04-01 16:46:21 +0100

    update on heading

--- src/components/atoms/Heading.jsx
+++ src/components/atoms/Heading.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Heading = () => {
-  return (
-    <div>Heading</div>
-  )
+export const Heading = ({ children, className, level }) => {
+  switch (level) {
+    case 1:
+      return <h1 className={clsx(styles.levelOne, className)}>{children}</h1>;
+    case 2:
+      return <h2 className={clsx(styles.levelTwo, className)}>{children}</h2>;
+    case 3:
+      return <h3 className={clsx(styles.levelThree, className)}>{children}</h3>;
+    case 4:
+      return <h4 className={clsx(styles.levelFour, className)}>{children}</h4>;
+    case 5:
+      return <h5 className={clsx(styles.levelFive, className)}>{children}</h5>;
+    case 6:
+      return <h6 className={clsx(styles.levelSix, className)}>{children}</h6>;
+
+    default:
+      return <h6 className={clsx(styles.levelSix, className)}>{children}</h6>;
+      break;
 }
 
-export default Heading
+}
