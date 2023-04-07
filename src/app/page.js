7dc05de2d4cd4c425dfc2017e15ba506fa06edfc
DevSenpai <devsenpai09@gmail.com>
2023-04-07 12:47:56 +0100

    fix merge conflict on page file

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-import { Inter } from "next/font/google";
+import { Archivo } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return (
-    <main>
-      <h1>This is the home page.</h1>
-=======
-import Image from 'next/image'
-import { Archivo } from 'next/font/google'
-import styles from './page.module.css'
-
-const Archivo = Archivo({ subsets: ['latin'] })
+const Archivo = Archivo({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     
->>>>>>> 1a6299ccd0d2d34b4fd848fdda2e84b1b4a9a90c
+      <h1>This is the home page.</h1>
     </main>
   );
 }
