867f735cdab3fb7fe0d76a62bc4532da015709ca
stephen <nwabuezesc@gmail.com>
2023-04-07 14:29:40 +0100

    creating the swap section

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import { Archivo } from "next/font/google";
 
 const archivo = Archivo({ subsets: ["latin"] });
 import TextSlider from "@/components/molecules/TextSlider";
+import Swap from "@/components/molecules/Swap";
 
 export default function Home() {
   return (
     <main className={archivo.className}>
       <h1>This is the home page.</h1>
       <TextSlider />
+      <Swap />
     </main>
   );
 }
---
+++ src/components/molecules/Swap.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Swap = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4 place-items-center h-80">
+      <div>Swap form</div>
+      <div>Image</div>
+    </div>
+  );
+};
+
+export default Swap;
