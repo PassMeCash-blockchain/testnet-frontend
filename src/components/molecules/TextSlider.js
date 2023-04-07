f307245eabf88e6be0f28d171db0c8c7107c298b
stephen <nwabuezesc@gmail.com>
2023-04-07 13:00:53 +0100

    creating the landing page marquee slider structure

---
+++ src/components/molecules/TextSlider.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TextSlider = () => {
+  return (
+    // text-wrapper
+    <div className="flex">
+      <div className="flex-none">01</div>
+      <div className="flex-auto w-64 ...">02</div>
+      <div className="flex-auto w-32 ...">03</div>
+    </div>
+  );
+};
+
+export default TextSlider;
