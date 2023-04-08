9168ab8c268ae113a944608de934a78515cac35f
Nonso Nwabueze <60317985+nwabuezenonso@users.noreply.github.com>
2023-04-08 16:51:03 +0100

    Update libraries.jsx

--- LandingPage/src/components/atoms/libraries.jsx
+++ LandingPage/src/components/atoms/libraries.jsx
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-import clsx from "clsx";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-=======
 import Image from "next/image";
 import clsx from 'clsx';
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import client from "../../../apolloClient";
->>>>>>> 9f5c54d404f7422908be4e5d8b89551f732dc037
 
 export const clsxes = clsx;
 export const ApolloClients = ApolloClient;
 export const InMemoryCaches = InMemoryCache;
 export const gqls = gql;
-<<<<<<< HEAD
-=======
 export const clients = client;
->>>>>>> 9f5c54d404f7422908be4e5d8b89551f732dc037
