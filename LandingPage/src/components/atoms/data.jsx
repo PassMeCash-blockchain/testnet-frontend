0da835765d54dbc396eef2d63dd4dbcc57e60807
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2023-04-08 09:44:53 +0100

    Update data.jsx

--- LandingPage/src/components/atoms/data.jsx
+++ LandingPage/src/components/atoms/data.jsx
@@ -3,23 +3,22 @@ export async function getStaticProps() {
        uri: 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clg5fdn18061201t38p7oe6lc/master',
        cache: new InMemoryCache(),
     });
+    const headerData = await client.query({
+    query: gql`
+      query MyQuery {
+  headerSection(where: {id: "clg6omvqs0lw30blj3t5l3qhj"}) {
+    header
+  }
+}
+    `,
+   });
+   const allHeaderData = headerData.data.header;
     return {
-       props: {},
+       props: {
+         allHeaderData,
+       },
     };
    }
 
-   const headerData = await client.query({
-    query: gql`
-       query ProductsQuery {
-          products {
-             id
-             name
-             slug
-             price
-             image {
-                url
-             }
-          }
-       }
-    `,
-   });
+   
+ 
