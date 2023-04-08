1164a31d5fcb66aaf2fa40c190a8b2d4ddc58bdb
Constance Osarodion Etiosa <54332180+Jonstance@users.noreply.github.com>
2023-04-08 13:10:55 +0100

    update

--- LandingPage/src/components/atoms/data.jsx
+++ LandingPage/src/components/atoms/data.jsx
@@ -1,4 +1,4 @@
-export async function getStaticProps() {
+export default async function getStaticProps() {
     const client = new ApolloClient({
        uri: 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clg5fdn18061201t38p7oe6lc/master',
        cache: new InMemoryCache(),
--- LandingPage/src/components/molecules/WhatWeDoSection.jsx
+++ LandingPage/src/components/molecules/WhatWeDoSection.jsx
@@ -7,16 +7,13 @@ const WhatWeDoSection = (allWhatWeDoData) => {
   return (
     <div className="flex flex-col gap-2 justify-center items-center lg:justify-start lg:items-start">
       <Heading level={4} className="border-b-4  border-greyColor px-4 lg:px-0">
-        {}
+        {allWhatWeDoData.wwdTitle}
       </Heading>
       <Heading level={1} className="">
         Learn More About Us
       </Heading>
       <Text className={`text-[16px] w-[400px]`}>
-        PassMeCash is an exchange platform that connects users to the nearest
-        agent, in exchange for cash or crypto. Our mission is to bridge the gap
-        between our users and access to their funds through the world's most
-        extensive financial system.
+      {allWhatWeDoData.wwdDescription}
       </Text>
       <Button className="w-full bg-primaryColor text-white px-2 py-2">Learn More</Button>
     </div>
