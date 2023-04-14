3570fb6bd7bdc4bb3ca5ad97441b2c3ed96e0b20
favour wilson <ebrusikefavour@gmail.com>
2023-04-14 12:09:28 +0100

    Fix

--- pmc-account_old/src/components/atoms/libraries.jsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import clsx from 'clsx';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-
-export const Images = Image;
-export const clsxes = clsx;
-export const ApolloClients = ApolloClient;
-export const InMemoryCaches = InMemoryCache;
-export const gqls = gql;
