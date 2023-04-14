f4818b6aa1f61134259da2a146d18b98f3df397a
favour wilson <ebrusikefavour@gmail.com>
2023-04-14 12:06:58 +0100

    Fix

---
+++ pmc-account_old/src/components/Organisms/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Heading } from '../atoms/Heading'
+import inputField from './../atoms/InputField';
+
+const Register = () => {
+  return (
+      <div>
+          <Heading level={2}>Create Account</Heading>
+          <inputField/>
+    </div>
+  )
+}
+
+export default Register
