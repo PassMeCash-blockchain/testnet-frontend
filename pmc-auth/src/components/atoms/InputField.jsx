fa6d3d3357d2515cb40d10e107e168b06acb9e59
Nonso Nwabueze <60317985+nwabuezenonso@users.noreply.github.com>
2023-04-14 20:42:13 +0100

    Update InputField.jsx

--- pmc-auth/src/components/atoms/InputField.jsx
+++ pmc-auth/src/components/atoms/InputField.jsx
@@ -4,7 +4,7 @@ import { Images } from './libraries'
 const InputField = ({onChange,className,type, icon, hasIcon=!1,placeHolder}) => {
   return (
     <>
-      <input type={type} placeholder={placeHolder} className={`${className} bg-none rounded-md static`} onchange={onChange} />
+      <input type={type} placeholder={placeHolder} className={`${className} bg-none rounded-md static`} onChange={onChange} />
       {!!hasIcon && 
          <Images className={` absolute top-[22%] left-[20rem] lg:left-[22rem] `} src={icon} width={20} height={5}/>
       }
