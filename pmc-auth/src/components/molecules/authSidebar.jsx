7ad2bd0c00c5ed3cf1ad624dc14d46e0fadf5c4e
favour wilson <ebrusikefavour@gmail.com>
2023-04-14 12:42:41 +0100

    tailwind config

---
+++ pmc-auth/src/components/molecules/authSidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Heading } from '../atoms/Heading'
+
+export const authSidebar = () => {
+  return (
+      <div className='grid place-items-center'>
+          <Heading level={1} className={`text-primaryColor`}/>
+    </div>
+  )
+}
--- pmc-auth/tailwind.config.js
+++ pmc-auth/tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/**/*.{html,jsx,js}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primaryColor: "#874CF6",
+        secondaryColor: "#0B011D",
+        inputColor: "#F5F5F5",
+        warningColor: "#F3AF0A",
+        dangerColor: "#EA4335",
+        processingColor: "#4564D6",
+        greenColor: "#0AC18E",
+        acctColor:"#F2ECFE"
+      },
+      keyframes: {
+        moveRTL: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        moveRTL: "moveRTL 6000ms linear infinite",
       },
     },
   },
   plugins: [],
-}
+};
