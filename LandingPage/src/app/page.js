773fd9027a8d249f6854caed761a71b6003150b4
DevSenpai <devsenpai09@gmail.com>
2023-04-24 14:17:36 +0100

    update service section on landing page

--- LandingPage/src/app/page.js
+++ LandingPage/src/app/page.js
@@ -15,8 +15,8 @@ export default async function Home() {
       <Hero />
       {/* <TextSlider /> */}
       {/* <Swap /> */}
-      {/* <Services /> */}
-      {/* <WhatWeDo /> */}
+      <Services />
+      <WhatWeDo />
     </main>
   );
 }
--- LandingPage/src/components/organisms/Services.jsx
+++ LandingPage/src/components/organisms/Services.jsx
@@ -1,92 +1,50 @@
 import ServiecesSection from "../templates/ServiecesSection";
-import { clients, gqls } from "../atoms/libraries";
-
-export async function fetchData() {
-  const { data: servicesData } = await clients.query({
-    query: gqls`
-    query MyQuery {
-      serviceSection(where: {id: "clgq8s6ai66ua0bjvfughxume"}) {
-        servicesTitle
-        s1Btn
-        s1BtnLink
-        s1Description
-        s1Title
-        s2Btn
-        s2BtnLink
-        s2Description
-        s2Title
-        s3Btn
-        s3BtnLink
-        s3Description
-        s3Title
-        s3Image {
-          url
-        }
-        s2Image {
-          url
-        }
-        s1Image {
-          url
-        }
-      }
-    }    
-    `,
-  });
-  const allServicesData = servicesData.serviceSection;
-  return {
-    allServicesData,
-  };
-}
 
 const Services = async () => {
-  const res = await fetchData();
-
   const schema = {
-    header: res.allServicesData.servicesTitle,
+    header: "Our Services",
     sections: [
       {
-        img: res.allServicesData.s1Image.url,
+        img: "/assets/service-img1.png",
         icon: "/assets/service-icon1.svg",
-        title: res.allServicesData.s1Title,
-        text: res.allServicesData.s1Description,
-        btn: { text: res.allServicesData.s1Btn, link: "" },
+        title: "Request Money",
+        text: "We understand how difficult it is to get your fund when you try to make a withdrawal. That is why we are making it seamless for our users to make a quick request of their money that can be delivered to you from any of our close agents by bank transfer or a meeting point.",
+        btn: { text: "Make Request", link: "" },
       },
       {
-        img: res.allServicesData.s2Image.url,
+        img: "/assets/service-img2.png",
         icon: "/assets/service-icon2.svg",
-        title: res.allServicesData.s2Title,
-        text: res.allServicesData.s2Description,
-        btn: { text: res.allServicesData.s2Btn, link: "" },
+        title: "Order for spray bills",
+        text: "We understand how difficult it is to get lower currency bills to use in spraying celebrants at parties and other activities. That is why we are making it seamless for our users to request for any of our currency bills and it will be delivered to you through any of our closest agents.",
+        btn: { text: "Make Request", link: "" },
       },
       {
-        img: res.allServicesData.s3Image.url,
+        img: "/assets/service-img3.png",
         icon: "/assets/service-icon3.svg",
-        title: res.allServicesData.s3Title,
-        text: res.allServicesData.s3Description,
-        btn: { text: res.allServicesData.s3Btn, link: "" },
+        title: "Transfer & Receive Money",
+        text: "We understand how stressful it is to get cash at hand when money is sent to you. That is why we are making it stressless to send and receive money instantly via cash or cryptocurrency.",
+        btn: { text: "Get started", link: "" },
       },
     ],
   };
 
   return (
-    <SectionSpacing className="md:px-0">
-      <div className="grid gap-10 md:gap-0">
-        {schema.sections.map((section, i) => (
-          <div key={i}>
-            <ServiecesSection
-              header={schema.header}
-              img={section.img}
-              icon={section.icon}
-              title={section.title}
-              text={section.text}
-              btnText={section.btn.text}
-              btnLink={section.btn.link}
-              inverted={i === 1 ? true : false}
-            />
-          </div>
-        ))}
-      </div>
-    </SectionSpacing>
+    <div className="grid gap-10 page-space mt-10 md:mt-20 md:gap-0">
+      {schema.sections.map((section, i) => (
+        <div key={i}>
+          <ServiecesSection
+            header={schema.header}
+            img={section.img}
+            icon={section.icon}
+            title={section.title}
+            text={section.text}
+            btnText={section.btn.text}
+            btnLink={section.btn.link}
+            inverted={i === 1 ? true : false}
+          />
+        </div>
+      ))}
+    </div>
   );
 };
 
