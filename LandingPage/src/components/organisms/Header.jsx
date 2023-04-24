3b2961c8bca34e54ad5bdc8cfc643a1c7410bde1
DevSenpai <devsenpai09@gmail.com>
2023-04-24 12:41:53 +0100

    update header responsiveness on landing page

--- LandingPage/src/components/organisms/Header.jsx
+++ LandingPage/src/components/organisms/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   ];
 
   return (
-    <div className="grid grid-cols-[1fr_auto] page-space font-medium text-base items-center pt-5 md:pt-5">
+    <div className="grid grid-cols-[1fr_auto] container mx-auto page-space font-medium text-base items-center pt-5 md:pt-5">
       <div className="flex space-x-8 items-center">
         <Logo width="w-20" />
         <HeaderNav navs={navs} />
