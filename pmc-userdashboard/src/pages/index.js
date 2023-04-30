ea9a3f0484736ade1a3ae75cc0f7950a6ad792a3
stephen <nwabuezesc@gmail.com>
2023-04-30 16:52:56 +0100

    creating the QR code

--- pmc-userdashboard/src/pages/index.js
+++ pmc-userdashboard/src/pages/index.js
@@ -4,6 +4,7 @@ import UserBalance from "@/components/molecules/UserBalance";
 import UserTransactions from "@/components/molecules/UserTransactions";
 import Header from "@/components/organisms/Header";
 import MobileMenu from "@/components/organisms/MobileMenu";
+import Image from "next/image";
 
 const archivo = Archivo({ subsets: ["latin"] });
 
@@ -23,6 +24,8 @@ export default function Home() {
         {/* <UserBalance />
         <UserTransactions /> */}
         <MobileMenu />
+
+        <Image src="/SVG/qr-code.svg" width={250} height={250} alt="QR code" />
       </PageLayout>
     </main>
   );
