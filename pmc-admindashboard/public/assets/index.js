f745bf2478453f9afb212b7f0b0bd0e48661b879
favour wilson <ebrusikefavour@gmail.com>
2023-05-05 13:18:20 +0100

    pmc admin

--- pmc-admindashboard/public/assets/index.js
+++
@@ -1,6 +0,0 @@
-import { MyWallets, Home2, notify, Request, PayBills, Transactions, user, VirtualCard } from '/assets/';
-
-export const Images = {
-    MyWallets, Home2, notify, Request, PayBills, Transactions, user, VirtualCard
-}
-
--- pmc-admindashboard/src/components/atoms/Label.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Label = ({className,Children}) => {
-  return (
-      <label className={className}>{ Children}</label>
-  )
-}
-
-export default Label
