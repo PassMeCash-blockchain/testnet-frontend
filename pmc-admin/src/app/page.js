c28a4ef59e857f92fb90e89f76a889ca34ed9a4d
stephen <nwabuezesc@gmail.com>
2023-05-06 13:30:58 +0100

    fixing merge input

--- pmc-admin/src/app/page.js
+++ pmc-admin/src/app/page.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
 import Barchart from "@/components/molecules/charts/Barchart";
-=======
-import ChartCard from "@/components/molecules/ChartCard";
-import Image from "next/image";
 
 const page = () => {
   return (
