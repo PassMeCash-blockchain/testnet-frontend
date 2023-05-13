51dde149142219b214e6029d8077f24274194e3e
DevSenpai <devsenpai09@gmail.com>
2023-05-13 21:56:54 +0100

    create crypto wallets page

---
+++ pmc-userdashboard/src/components/molecules/SelectedCryptoWallet.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Recieve from "../organisms/Recieve";
+import Swap from "../organisms/Swap";
+import Send from "../organisms/Send";
+import Icons from "../atoms/Icons";
+import Image from "next/image";
+
+function SelectedCryptoWallet({ selectedWallet, setSelectedWallet }) {
+  return (
+    <div className="bg-[#F4F4F4] p-5 rounded-md">
+      {/* Coin and Close BTN */}
+      <div className="flex justify-between">
+        {/* Coin */}
+        <div className="w-12 h-12 rounded-full overflow-hidden relative">
+          <Image
+            src="/assets/PolygonBadge.svg"
+            width={1000}
+            height={1000}
+            alt="Crypto Icon"
+          />
+        </div>
+
+        {/* Close BTN */}
+        <button
+          className="rounded-full w-7 h-7 bg-[rgba(255,255,255,0.7)] grid place-items-center"
+          onClick={() => setSelectedWallet({})}
+        >
+          <Icons type="close" className="text-lg" />
+        </button>
+      </div>
+
+      {/* Crypto Balance */}
+      <div className="mt-5">
+        <p className="font-semibold text-[22px]">{selectedWallet.coin}</p>
+        <p className="text-4xl font-semibold leading-[30px  ]">
+          {selectedWallet.amount}.00
+        </p>
+      </div>
+
+      {/* CTAs */}
+      <div className="grid grid-cols-[auto_auto_auto] gap-10 w-max mt-5">
+        {/* Receive */}
+        <Recieve />
+
+        {/* Swap */}
+        <Swap />
+
+        {/* Send */}
+        <Send />
+      </div>
+    </div>
+  );
+}
+
+export default SelectedCryptoWallet;
--- pmc-userdashboard/src/pages/crypto-wallets.jsx
+++ pmc-userdashboard/src/pages/crypto-wallets.jsx
@@ -1,5 +1,38 @@
+import PickCryptoWallet from "@/components/molecules/PickCryptoWallet";
+import SelectedCryptoWallet from "@/components/molecules/SelectedCryptoWallet";
+import UserTransactions from "@/components/molecules/UserTransactions";
+import PageLayout from "@/components/templates/PageLayout";
+import { useState } from "react";
+
 const CryptoWallets = () => {
-  return <div>CryptoWallets</div>;
+  const cryptoWallets = [
+    { coin: "MATIC", logo: "/assets/PolygonBadge.svg", amount: 500 },
+    { coin: "BNB", logo: "/assets/PolygonBadge.svg", amount: 400 },
+    { coin: "BTC", logo: "/assets/PolygonBadge.svg", amount: 300 },
+    { coin: "DODGE", logo: "/assets/PolygonBadge.svg", amount: 700 },
+    { coin: "TRON", logo: "/assets/PolygonBadge.svg", amount: 500 },
+    { coin: "PI", logo: "/assets/PolygonBadge.svg", amount: 10000 },
+  ];
+  const [selectedWallet, setSelectedWallet] = useState({});
+
+  return (
+    <PageLayout>
+      <div>
+        {Object.keys(selectedWallet).length === 0 ? (
+          <PickCryptoWallet
+            cryptoWallets={cryptoWallets}
+            setSelectedWallet={setSelectedWallet}
+          />
+        ) : (
+          <SelectedCryptoWallet
+            selectedWallet={selectedWallet}
+            setSelectedWallet={setSelectedWallet}
+          />
+        )}
+      </div>
+      <UserTransactions />
+    </PageLayout>
+  );
 };
 
 export default CryptoWallets;
